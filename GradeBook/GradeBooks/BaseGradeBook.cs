0d055713aa86d5edd572063bbf110c9a06e49410
Evfra <adiwabik@onet.pl>
2022-03-31 11:28:51 +0200

    update BaseGradeBook.GetGpa

--- GradeBook/GradeBooks/BaseGradeBook.cs
+++ GradeBook/GradeBooks/BaseGradeBook.cs
@@ -108,36 +108,28 @@ namespace GradeBook.GradeBooks
 
         public virtual double GetGPA(char letterGrade, StudentType studentType)
         {
+            var gpa = 0;
             switch (letterGrade)
             {
                 case 'A':
-                    return 4;
-                    if (IsWeighted && (studentType == StudentType.Honors || studentType == StudentType.DualEnrolled))
-                        return 5;
-                    else
-                        return 4;
+                    gpa = 4;
+                    break;
                 case 'B':
-                    return 3;
-                    if (IsWeighted && (studentType == StudentType.Honors || studentType == StudentType.DualEnrolled))
-                        return 4;
-                    else
-                        return 3;
+                    gpa = 3;
+                    break;
                 case 'C':
-                    return 2;
-                    if (IsWeighted && (studentType == StudentType.Honors || studentType == StudentType.DualEnrolled))
-                        return 3;
-                    else
-                        return 2;
+                    gpa = 2;
+                    break;
                 case 'D':
-                    return 1;
-                    if (IsWeighted && (studentType == StudentType.Honors || studentType == StudentType.DualEnrolled))
-                        return 2;
-                    else
-                        return 1;
+                    gpa = 1;
+                    break;
                 case 'F':
-                    return 0;
+                    gpa = 0;
+                    break;
             }
-            return 0;
+            if (IsWeighted && (studentType == StudentType.Honors || studentType == StudentType.DualEnrolled))
+                gpa++;
+            return gpa;
         }
 
         public virtual void CalculateStatistics()
